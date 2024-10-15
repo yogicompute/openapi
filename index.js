e626a06a0a80419df32f53ff7cf109b5ebe76622
Yogender Sharma <yogender.sharam23@lpu.in>
2024-10-15 18:22:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const dotenv = require('dotenv').config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+async function generateContent(userInput) {
+    try {
+        const result = await model.generateContent(userInput);
+        let responseText = result.response.text();
+        return responseText.trim();
+    } catch (error) {
+        console.error("Error generating content:", error);
+        throw error;
+    }
+}
+
+app.get('/send', async (req, res) => {
+    const userInput = `${req.query.input} the output should be a working cpp code without any comments`;
+
+
+    if (!userInput) {
+        return res.status(400).json({ error: "Missing input parameter" });
+    }
+
+    try {
+        const responseFromApi = await generateContent(userInput);
+        res.status(200).json({ result: responseFromApi });
+    } catch (error) {
+        res.status(500).json({ error: "Error generating content" });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
